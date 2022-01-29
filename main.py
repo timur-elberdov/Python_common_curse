54101eaec8cf8cc217e6a9905afcba0d345731c2
Timur Elberdov <timur.elberdov@gmail.com>
2022-01-30 00:27:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Это главный файл моего проекта по курсу "Python.Общий курс" на GeekBrains.
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('Timur')
+
