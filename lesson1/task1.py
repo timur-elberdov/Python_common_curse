754b220fba7b12d923e4ee5a63edeff88fb3f8bf
Timur Elberdov <timur.elberdov@gmail.com>
2022-02-05 19:24:42 +0300

    Solve task1

---
+++ lesson1/task1.py
@@ -0,0 +1,20 @@
+"""
+Поработайте с переменными, создайте несколько, выведите на экран.
+Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+"""
+
+var_str1 = 'Hello world'
+var_str2 = 'Hello you too'
+
+print(var_str1, '\n', var_str2)
+
+var_int1 = 55
+var_int2 = 11
+var_int3 = var_int2 + var_int1
+
+print(f'your vars: {var_int1}, {var_int2}, {var_int3}')
+
+var_numb = input('Введите число: ')
+var_str = input('Введите строку: ')
+print(f'Ваше число: {var_numb}, ваша строка: {var_str}')
+
