49dad9dde49d2376d5f292d7f6cc663486166903
Timur Elberdov <timur.elberdov@gmail.com>
2022-02-06 16:23:04 +0300

    Modified task4
    Добавил проверку, что пользователь ввёл число. Решил задачу с использованием арифметических операций, предыдущее решение закоментил.

--- lesson1/task4.py
+++ lesson1/task4.py
@@ -3,12 +3,27 @@
 Для решения используйте цикл while и арифметические операции.
 """
 
-user_numb = input('Введите целое число:')
+while True:
+    user_numb = input('Введите целое число:')
+    if user_numb.isdigit():
+        user_numb = int(user_numb)
+        break
+    else:
+        print('Вы не ввели целое положительное число')
+
+"""
 i = len(user_numb) - 1
 max_numb = 0
 while i >= 0:
     if int(user_numb[i]) > max_numb:
         max_numb = int(user_numb[i])
     i -= 1
+print(max_numb)
+"""
+max_numb = 0
+while user_numb > 0 and max_numb != 9:
+    if (user_numb % 10) > max_numb:
+        max_numb = user_numb % 10
+    user_numb //= 10
 
 print(max_numb)
