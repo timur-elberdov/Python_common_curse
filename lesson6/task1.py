067e66551eecec0fb85033543e1201486dc08298
Timur Elberdov <timur.elberdov@gmail.com>
2022-03-06 17:20:49 +0300

    Solve task1

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -15,26 +15,29 @@ from itertools import cycle
 class TrafficLight:
     def __init__(self):
         self.__colour = 'red'
-        self.__colours = cycle(['yellow', 'green','red'])
+        self.__colours = cycle(('yellow', 'green', 'red'))
         self.__off = False
 
-    def off(self):
-        self.off = True
-
-    def __swithc_light(self, light_time):
+    def __switch_light(self, light_time):
         start = time()
         while True:
             if time() - start >= light_time:
                 break
         self.__colour = next(self.__colours)
 
+    def __colour_check(self, prev_colour):
+        colour_check = ('red', 'yellow', 'green')
+        return colour_check[(colour_check.index(self.__colour) - 1)] == prev_colour
+
     def running(self, red_time=7, yellow_time=2, green_time=7):
         for time in cycle([red_time, yellow_time, green_time]):
+            prev_colour = self.__colour
             print(self.__colour)
-            self.__swithc_light(time)
-            if self.__off:
+            self.__switch_light(time)
+            if not self.__colour_check(prev_colour):
                 break
 
 
 a = TrafficLight()
 a.running()
+
